25ba1411979de26938e8689d1a7b7611e7dbb4cc
Evgeniy Moryakhin <mor.evg.alex@gmail.com>
2021-04-20 22:15:27 +0300

    add Course model

--- onboarding/courses/models.py
+++ onboarding/courses/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Course(models.Model):
+    STATUS_CHOICES = {
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    }
+
+    TYPES = {
+        ('test', 'Test'),
+        ('game', 'Game'),
+        ('doc', 'Document'),
+    }
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True)
+    is_mandatory = models.BooleanField(default=False)
+    description = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    type = models.CharField(max_length=10, choices=TYPES)
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
