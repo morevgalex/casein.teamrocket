fb755df1c151b8ff7ee45748b01867a3d4ac7aa6
Evgeniy Moryakhin <mor.evg.alex@gmail.com>
2021-04-21 18:13:00 +0300

    add profile page

--- onboarding/onboarding_core/urls.py
+++ onboarding/onboarding_core/urls.py
@@ -5,4 +5,5 @@ app_name = 'onboarding_core'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('profile/', views.profile, name='profile'),
 ]
--- onboarding/onboarding_core/views.py
+++ onboarding/onboarding_core/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'onboarding_core/main.html')
+
+
+def profile(request):
+    return render(request, 'onboarding_core/profile.html')
