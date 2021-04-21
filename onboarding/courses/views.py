4dfb4cfe6d61570decffec7f2eb007a3611e4ad2
Evgeniy Moryakhin <mor.evg.alex@gmail.com>
2021-04-21 14:51:46 +0300

    add controllers for course app

--- onboarding/courses/views.py
+++ onboarding/courses/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Course
 
-# Create your views here.
+
+def course_list(request):
+    courses = Course.publish.all()
+    return render(request, 'courses/course/list.html', {
+        'courses': courses
+    })
+
+
+def course_detail(request, slug):
+    course = get_object_or_404(Course, slug=slug)
+    return render(request, 'courses/course/detail.html', {
+        'course': course
+    })
+
