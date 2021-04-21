ad9195021dafcae41e0aeaccb4ddebc28e260621
Evgeniy Moryakhin <mor.evg.alex@gmail.com>
2021-04-21 14:47:10 +0300

    add publish manager for Course model

--- onboarding/courses/models.py
+++ onboarding/courses/models.py
@@ -2,7 +2,15 @@ from django.db import models
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
 class Course(models.Model):
+    objects = models.Manager()
+    published = PublishedManager()
+
     STATUS_CHOICES = {
         ('draft', 'Draft'),
         ('published', 'Published'),
