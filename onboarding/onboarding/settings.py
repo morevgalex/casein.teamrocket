6a6d5deed766d12d9ce8c5dbec147a7308210e2f
Evgeniy <mor.evg.alex@gmail.com>
2021-04-21 14:43:09 +0000

    add requirements; allow all hosts

--- onboarding/onboarding/settings.py
+++ onboarding/onboarding/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-uv0qytjr95+dpf$0@+fo&$ajqiih^r2%)%oax#!xdiv%7f-^q^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
